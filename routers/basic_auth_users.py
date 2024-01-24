d7223a9eb9f0512c517a2e39298f341be4cb2c85
juanchocor <jazz1916.jdc@gmail.com>
2024-01-23 20:38:58 -0500

    refuerzo de login

--- routers/basic_auth_users.py
+++ routers/basic_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends,HTTPException,status
+from fastapi import FastAPI, Depends, HTTPException, status 
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -15,9 +15,9 @@ class User(BaseModel):
 class UserDB(User):
     password: str
 
-user_db = {
+users_db = {
     "juanchocor" : {
-        "username": "juanhocor",
+        "username": "juanchocor",
         "full_name": "Cordoba M",
         "email": "jazz1916@gmail.com",
         "disabled": False, 
@@ -40,29 +40,31 @@ user_db = {
 }
 
 def search_user(username: str):
-    if username in user_db:
-        return UserDB(users_db[username])
+    if username in users_db:
+        return UserDB(**users_db[username])
 
 async def current_user(token: str = Depends(oauth2)):
     user = search_user(token)
     if not user:
         raise HTTPException(
-            status_code=401, detail="el usuario no es correcto"
-        )
-
-
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales de autentificacion invalidas", 
+            headers={"www-Authenticate":"Bearer"})
+    return user
 
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
-    user_db = user_db.get(form.username)
+    user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(status_code=status, detail="el usuario no es correcto")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail="el usuario no es correcto")
 
     user = search_user(form.username)
     if not form.password == user.password:
         raise HTTPException(
-            status_code=400, detail="la contraseña no es correcta"
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="la contraseña no es correcta"
             )
 
     return {"access_token": user.username, "token_type": "bearer"}
